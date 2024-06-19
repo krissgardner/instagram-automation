cf88863e5cad58512bd784bc30ec105ea59c3d9e
Kriss Gardner <krissrobert12@gmail.com>
2024-06-19 19:25:42 +0300

    Update constants

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-const IDLE = 0;
-const STARTING = 1;
-const WAITING = 2;
-const WORKING = 3;
-const CLOSING = 4;
-const ERROR = 5;
-
-module.exports = { IDLE, STARTING, WAITING, WORKING, CLOSING, ERROR };
