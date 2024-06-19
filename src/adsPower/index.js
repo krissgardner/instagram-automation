e8226eeab4d0dd8c4f8224ef5b4fef87b424b21c
Kriss Gardner <krissrobert12@gmail.com>
2024-06-19 19:21:19 +0300

    Update index

--- src/adsPower/index.js
+++
@@ -1,7 +0,0 @@
-const AdsPowerManager = require("./AdsPowerManager");
-const credentials = require("../storage/credentials.json");
-
-const { adsPowerEndpoint, adsPowerApiKey } = credentials;
-const adsPowerManager = new AdsPowerManager(adsPowerEndpoint, adsPowerApiKey);
-
-module.exports = { adsPowerManager };
