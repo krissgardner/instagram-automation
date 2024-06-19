3c19f38ae430ba4d8367de8fc5db6e55d3317673
Kriss Gardner <krissrobert12@gmail.com>
2024-06-19 19:22:22 +0300

    Convert db index

--- src/db/index.js
+++
@@ -1,9 +0,0 @@
-const DBManager = require("./DBManager");
-const {join} = require("node:path");
-
-const storagePath = join(__dirname, "./storage.json");
-const dbManager = new DBManager(storagePath);
-
-dbManager.checkIntegrity()
-
-module.exports = dbManager;
